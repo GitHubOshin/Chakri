f63914734c6a6ebc766ede33e75702fa5f995642
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-09 23:35:57 +0700

    Add screen & move colours into extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,39 @@ module.exports = {
   theme: {
     screens: {
       mobile: '375px',
-      laptop: '1920px'
+      mobileS: '320px',
+      laptop: '1920px',
+      screen1280: '1280px'
     },
-    colors: {
-      darkBlue: '#183B56',
-      darkGrey: '#3F475C',
-      grey: '#838995',
-      grey001: '#5A7184',
-      grey002: '#92AABD',
-      grey003: '#acb8c1',
-      grey004: '#C4C4C4',
-      grey005: '#ecedef',
-      grey006: '#e8ebee',
-      grey007: '#e9edeb',
-      pumpkin: '#E95432',
-      orange: '#FF8345',
-      apricot: '#fda87c',
-      plango: '#FFB583',
-      bgCircleOrange: 'rgb(255 244 236)',
-      bgSubscribe: '#F3F6FB',
-      white: '#FFFFFF',
-      green: '#7BC678',
-      divPink: 'rgba(233, 84, 50, 0.1)'
-    },
+
     fontFamily: {
       dmSans: ['DM Sans'],
       poppins: ['Poppins']
     },
-    extend: {}
+    extend: {
+      colors: {
+        darkBlue: '#183B56',
+        darkGrey: '#3F475C',
+        grey: '#838995',
+        grey001: '#5A7184',
+        grey002: '#92AABD',
+        grey003: '#acb8c1',
+        grey004: '#C4C4C4',
+        grey005: '#ecedef',
+        grey006: '#e8ebee',
+        grey007: '#e9edeb',
+        pumpkin: '#E95432',
+        orange: '#FF8345',
+        apricot: '#fda87c',
+        plango: '#FFB583',
+        bgCircleOrange: 'rgb(255 244 236)',
+        bgSubscribe: '#F3F6FB',
+        white: '#FFFFFF',
+        green: '#7BC678',
+        divPink: 'rgba(233, 84, 50, 0.1)'
+      }
+    }
   },
   plugins: []
 }
+
