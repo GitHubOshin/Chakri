4cbc98c8897151125178d5d719fe8fd2f729ace4
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-10 23:54:57 +0700

    Update PeopleHaveFoundJobFirstPage.js

--- src/components/firstPage/PeopleHaveFoundJobFirstPage.js
+++ src/components/firstPage/PeopleHaveFoundJobFirstPage.js
@@ -1,5 +1,23 @@
 function PeopleHaveFoundJobFirstPage() {
-  return <div className=""></div>
+  return (
+    <div className="relative laptop:w-[960px] laptop:h-[840px] laptop:bg-fuchsia-200">
+      <img
+        alt="Avocado Background"
+        src="images/otherImages/vector-green.svg"
+        className="absolute mobileS:w-[228.29px] mobileS:h-[228.12px] laptop:w-[550.12px] laptop:h-[549.71px]"
+      />
+      <img
+        alt="Line"
+        src="images/otherImages/vector1.svg"
+        className="absolute laptop:w-[594.32px] laptop:h-[593.88px] mobileS:w-[246.63px] mobileS:h-[246.45px]"
+      />
+      <img
+        alt="Dots"
+        src="images/otherImages/green-dots.svg"
+        className="absolute mobileS:w-[51.12px] mobileS:h-[90.1px] laptop:w-[123.19px] laptop:h-[217.13px]"
+      />
+    </div>
+  )
 }
 
 export default PeopleHaveFoundJobFirstPage
