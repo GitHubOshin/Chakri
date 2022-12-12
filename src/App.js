472b3bd40e4c2e77933d43b1c138be358fc42134
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-12 20:40:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import FirstPage from './pages/FirstPage'
 import Categories from './pages/Categories'
+import Logos from './pages/Logos'
 function App() {
   return (
     <div className="App">
       <FirstPage />
+      <Logos />
       <Categories />
     </div>
   )
