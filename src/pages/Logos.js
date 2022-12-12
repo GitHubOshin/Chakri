205cfd8fac9a722211297439b6756aed8f2ae47e
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-12 20:39:32 +0700

    Update Logos.js

--- src/pages/Logos.js
+++ src/pages/Logos.js
@@ -1,33 +1,41 @@
 function Logos() {
   return (
-    <div className="mobile:w-[375px] mobile:h-[302.61px] laptop:w-[1920px] laptop:h-[234px] flex items-center justify-center">
-      <div className="mobile:w-[327px] mobile:h-[142.61px] laptop:w-[1304px] laptop:h-[74px] flex laptop:items-center laptop:justify-between mobile:flex-wrap">
-        <img
-          alt="Logo: Caley Peace"
-          src="images/logos/caley-peace.svg"
-          className="mobile:w-[81.06px] mobile:h-[33.54px] laptop:w-[145px] laptop:h-[60px]"
-        />
-        <img
-          alt="Logo: Design Studio"
-          src="images/logos/design-studio.svg"
-          className="mobile:w-[81.06px] mobile:h-[33.54px] laptop:w-[145px] laptop:h-[60px]"
-        />
-        <img
-          alt="Logo: Harrison"
-          src="images/logos/harrison.svg"
-          className="mobile:w-[81.62px] mobile:h-[26.83px] laptop:w-[146px] laptop:h-[48px]"
-        />
-        <img
-          alt="Logo: Barbecue"
-          src="images/logos/barbecue.svg"
-          className="mobile:w-[76.03px] mobile:h-[41.37px] laptop:w-[136px] laptop:h-[74px]"
-        />
-        <img
-          alt="Logo: Design Exclusive"
-          src="images/logos/design-exclusive.svg"
-          className="mobile:w-[66.53px] mobile:h-[32.42px] laptop:w-[119px] laptop:h-[58px]"
-        />
-        <img alt="" src="images/logos/" />
+    <div className="w-full mobile:h-[302.64px] screen1280:h-[234px] flex items-center justify-center">
+      <div className=" w-full mobile:w-[327px] mobile:h-[142.61px] screen750:w-[750px] screen1280:w-[1304px] laptop:w-[1304px] screen1280:px-[0px]  mobile:px-[24px] flex screen1280:justify-between screen750:gap-[40px] screen1280:gap-0 ">
+        <div className=" flex w-full screen1280:w-[743px] mobile:flex-col screen750:flex-row mobile:items-center mobile:justify-between  screen750:gap-[44px] ">
+          <img
+            alt="Logo: Caley Peace"
+            src="images/logos/caley-peace.svg"
+            className="mobile:w-[81.06px] mobile:h-[33.54px] screen1280:w-[145px] screen1280:h-[60px]"
+          />
+          <img
+            alt="Logo: Design Studio"
+            src="images/logos/design-studio.svg"
+            className="mobile:w-[81.06px] mobile:h-[33.54px] screen1280:w-[145px] screen1280:h-[60px]"
+          />
+          <img
+            alt="Logo: Harrison"
+            src="images/logos/harrison.svg"
+            className="mobile:w-[81.62px] mobile:h-[26.83px] screen1280:w-[145px] screen1280:h-[60px]"
+          />
+        </div>
+        <div className=" flex w-full  screen1280:w-[408px] mobile:flex-col mobile:justify-between  screen750:flex-row mobile:items-center  screen750:gap-[44px] screen1280:gap-0">
+          <img
+            alt="Logo: Barbecue"
+            src="images/logos/barbecue.svg"
+            className="mobile:w-[76.03px] mobile:h-[41.37px] screen1280:w-[145px] screen1280:h-[60px]"
+          />
+          <img
+            alt="Logo: Design Exclusive"
+            src="images/logos/design-exclusive.svg"
+            className="mobile:w-[66.53px] mobile:h-[32.42px] screen1280:w-[145px] screen1280:h-[60px]"
+          />
+          <img
+            alt="Logo: Creative Design"
+            src="images/logos/creative-design.svg"
+            className="screen1280:hidden screen750:inline mobile:w-[50px] mobile:h-[37px]"
+          />
+        </div>
       </div>
     </div>
   )
