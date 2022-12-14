44dccbf6f60545e2657917375d4cd5d0a849be35
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-14 14:10:45 +0700

    Create BrowseJobsByCategory.js for Categories page

---
+++ src/data/BrowseJobsByCategory.js
@@ -0,0 +1,53 @@
+const browseJobsByCategory = [
+  {
+    alternative: 'Speaker',
+    orangeIcon: 'images/icons/speaker.svg',
+    whiteIcon: 'images/white-icon/whiteSpeaker.svg',
+    category: 'Digital Marketing',
+    jobsOpened: 143
+  },
+  {
+    alternative: 'Pen',
+    orangeIcon: 'images/icons/pen.svg',
+    whiteIcon: 'images/white-icon/whitePen.svg',
+    category: 'Art & Design',
+    jobsOpened: 110
+  },
+  {
+    alternative: 'Dollar Sign',
+    orangeIcon: 'images/icons/dollar-sign.svg',
+    whiteIcon: 'images/white-icon/whiteDollar-sign.svg',
+    category: 'Accounting',
+    jobsOpened: 158
+  },
+  {
+    alternative: 'Camera',
+    orangeIcon: 'images/icons/camera.svg',
+    whiteIcon: 'images/white-icon/whiteCamera.svg',
+    category: 'Photography',
+    jobsOpened: 95
+  },
+  {
+    alternative: 'Create',
+    orangeIcon: 'images/icons/create.svg',
+    whiteIcon: 'images/white-icon/whiteCreate.svg',
+    category: 'Copy Writing',
+    jobsOpened: 113
+  },
+  {
+    alternative: 'Headphone',
+    orangeIcon: 'images/icons/headphone.svg',
+    whiteIcon: 'images/white-icon/whiteHeadphone.svg',
+    category: 'Music',
+    jobsOpened: 55
+  },
+  {
+    alternative: 'Humans',
+    orangeIcon: 'images/icons/humans.svg',
+    whiteIcon: 'images/white-icon/whiteHumans.svg',
+    category: 'Human Resouces',
+    jobsOpened: 25
+  }
+]
+
+export default browseJobsByCategory
