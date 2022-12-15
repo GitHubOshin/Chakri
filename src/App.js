c192ccea3d0dbc6e9f283dfb4823f9be0b51cff1
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-15 23:50:57 +0700

    Add Services.js page in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import FirstPage from './pages/FirstPage'
 import Categories from './pages/Categories'
 import Logos from './pages/Logos'
+import Services from './pages/Services'
 function App() {
   return (
     <div className="App">
       <FirstPage />
       <Logos />
       <Categories />
+      <Services />
     </div>
   )
 }
