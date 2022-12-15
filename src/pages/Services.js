0ca47e2a677a21628d1e3f46f70f4f20111f0e94
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-15 23:54:55 +0700

    Update Services.js

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,5 +1,14 @@
+import ServicesPageBlockB from '../components/servicesPage/ServicesPageBlockB'
+import ServicesPageBlockA from '../components/servicesPage/ServicesPageBlockA'
+
+// #FCFCFC
 function Services() {
-  return <div className=""></div>
+  return (
+    <div className="w-full flex mobile:flex-col screen1280:flex-row screen1280:h-[1079.96px] mobile:h-[1492.56px] bg-red-400">
+      <ServicesPageBlockA />
+      <ServicesPageBlockB />
+    </div>
+  )
 }
 
 export default Services
