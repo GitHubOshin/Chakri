3ea7d5b6fef9aef65ea8b6716d59a1f58219bc93
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-17 23:54:14 +0700

    Update Services.js

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -4,9 +4,11 @@ import ServicesPageBlockA from '../components/servicesPage/ServicesPageBlockA'
 // #FCFCFC
 function Services() {
   return (
-    <div className="w-full flex mobile:flex-col screen1280:flex-row screen1280:h-[1079.96px] mobile:h-[1492.56px] bg-red-400">
-      <ServicesPageBlockA />
-      <ServicesPageBlockB />
+    <div className="flex items-center justify-center h-fit mobile:py-[80px]">
+      <div className=" max-w-[1421.07px] mobile:w-full flex mobile:flex-col mobile:items-center mobile:gap-[61.74px] screen1280:flex-row screen1280:gap-0 screen1280:justify-between screen1280:px-[70px] laptop:px-0 laptop:items-start">
+        <ServicesPageBlockA />
+        <ServicesPageBlockB />
+      </div>
     </div>
   )
 }
