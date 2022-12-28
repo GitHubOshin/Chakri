e0127fb71b80ecc033caf0c4a99fb6d1ffe91404
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-12-28 17:03:47 +0700

    FooterItems.js

---
+++ src/data/FooterItems.js
@@ -0,0 +1,65 @@
+export const services = [
+  {
+    link: '#',
+    content: 'Documentation'
+  },
+  {
+    link: '#',
+    content: 'Design'
+  },
+  {
+    link: '#',
+    content: 'Themes'
+  },
+  {
+    link: '#',
+    content: 'Illustrations'
+  }
+]
+
+export const company = [
+  {
+    link: '#',
+    content: 'About'
+  },
+  {
+    link: '#',
+    content: 'Terms'
+  },
+  {
+    link: '#',
+    content: 'Privacy Policy'
+  },
+  {
+    link: '#',
+    content: 'Careers'
+  }
+]
+
+export const jobInfo = [
+  {
+    link: '#',
+    content: 'Select'
+  },
+  {
+    link: '#',
+    content: 'Services'
+  },
+  {
+    link: '#',
+    content: 'Payment'
+  }
+]
+
+export const contact = [
+  { type: 'Call', contact: '971 5546 963' },
+  { type: 'Email', contact: 'example@gmail.com' },
+  { type: 'Address', contact: 'Dhaka, Bangladesh' }
+]
+
+export const socialMediaIcons = [
+  { iconName: 'Google', icon: 'images/icons/google.svg' },
+  { iconName: 'Twitter', icon: 'images/icons/twitter.svg' },
+  { iconName: 'Instagram', icon: 'images/icons/instagram.svg' },
+  { iconName: 'Linkedin', icon: 'images/icons/linkedin.svg' }
+]
