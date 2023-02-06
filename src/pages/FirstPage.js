4dad82d81ff864fb2484cd07b9efcdab3ac4bb33
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-02-06 18:17:45 +0700

    Update FirstPage.js

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -6,7 +6,7 @@ function FirstPage() {
   return (
     <div className="bg-[#f9fdfb] flex flex-col items-center screen1280:items-center">
       <HeaderFirstPage />
-      <div className="mobile:gap-y-[30px] mobile:flex mobile:flex-col mobile:items-center screen750:flex screen750:flex-row screen750:items-center screen750:justify-center">
+      <div className="mobile:gap-y-[30px] mobile:flex mobile:flex-col mobile:items-center screen1280:flex screen1280:flex-row screen750:items-center screen750:justify-center">
         <SearchingFirstPage />
         <PeopleHaveFoundJobFirstPage />
       </div>
