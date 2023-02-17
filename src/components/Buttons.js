51f158dc7fdd0c2ab6cefe8e56c5cbb3f735bae3
OSHIN <izkp44@gmail.com>
2023-02-17 15:27:05 +0700

    Create Buttons.js

---
+++ src/components/Buttons.js
@@ -0,0 +1,15 @@
+export function Register() {
+  return (
+    <button className="text-darkBlue screen1280:w-[104px] screen1280:h-[46px] screen1280:text-[12px] screen1280:font-medium laptop:w-[139px] laptop:h-[64px] laptop:font-medium laptop:text-[16px]">
+      Register
+    </button>
+  )
+}
+
+export function SignIn() {
+  return (
+    <button className="screen1280:bg-orange screen1280:text-white  screen1280:w-[104px] screen1280:h-[46px] screen1280:rounded-[7px] screen1280:text-[12px] screen1280:font-medium laptop:w-[139px] laptop:h-[64px] laptop:rounded-[10px] laptop:font-medium laptop:text-[16px]">
+      Sign In
+    </button>
+  )
+}
