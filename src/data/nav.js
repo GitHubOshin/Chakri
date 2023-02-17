ba82047466a69f3c903d748fc1b072200bb01afe
OSHIN <izkp44@gmail.com>
2023-02-17 15:28:54 +0700

    Create nav.js

---
+++ src/data/nav.js
@@ -0,0 +1,20 @@
+const nav = [
+  {
+    section: 'Category',
+    link: '#category'
+  },
+  {
+    section: 'Job',
+    link: '#job'
+  },
+  {
+    section: 'How it works',
+    link: '#how-it-works'
+  },
+  {
+    section: 'Testimonial',
+    link: '#testimonial'
+  }
+]
+
+export default nav
