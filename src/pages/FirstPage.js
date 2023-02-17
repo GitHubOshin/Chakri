28501645b778b01f2d0e3af3600495b4e996874e
OSHIN <izkp44@gmail.com>
2023-02-17 15:37:09 +0700

    Delete HeaderFirstPage and use Nav

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,12 +1,12 @@
-import HeaderFirstPage from '../components/firstPage/HeaderFirstPage'
+import Nav from '../components/Nav'
 import SearchingFirstPage from '../components/firstPage/SearchingFirstPage'
 import PeopleHaveFoundJobFirstPage from '../components/firstPage/PeopleHaveFoundJobFirstPage'
 
 function FirstPage() {
   return (
-    <div className="bg-[#f9fdfb] flex flex-col items-center screen1280:items-center">
-      <HeaderFirstPage />
-      <div className="mobile:gap-y-[30px] mobile:flex mobile:flex-col mobile:items-center screen1280:flex screen1280:flex-row screen750:items-center screen750:justify-center">
+    <div className="bg-[#f9fdfb]">
+      <Nav />
+      <div className="mobile:gap-y-[30px] mobile:flex mobile:flex-col mobile:items-center screen1280:flex screen1280:flex-row screen750:items-center screen750:justify-center mobile:mt-[100px] ">
         <SearchingFirstPage />
         <PeopleHaveFoundJobFirstPage />
       </div>
